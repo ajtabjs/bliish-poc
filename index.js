3be886bed6da9729cbd783603e3d49556569fe24
aj <134237782+ajtabjs@users.noreply.github.com>
2026-04-09 14:48:05 +0000

    axios stuff

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 const https = require('https');
 
 var user = "snapple"
@@ -5,3 +6,9 @@ var user = "snapple"
 const globalposts = "https://bliish.com/api/v1/posts"
 
 const wallposts = `https://bliish.com/api/v1/profiles/${user}/wall/posts`
+
+axios.post(globalposts, payload)
+.then(response => {
+console.log('cool status', response.status);
+console.log('data we be sendin', response.data);
+})
