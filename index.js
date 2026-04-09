4bdb431d89f199ef8dcca14e83fe06a20d3fc102
aj <134237782+ajtabjs@users.noreply.github.com>
2026-04-09 14:45:27 +0000

    setup basic api points for the bot

---
+++ index.js
@@ -0,0 +1,7 @@
+const https = require('https');
+
+var user = "snapple"
+
+const globalposts = "https://bliish.com/api/v1/posts"
+
+const wallposts = `https://bliish.com/api/v1/profiles/${user}/wall/posts`
