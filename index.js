2f35aea7ccc710907a56fcb89244471ad019ffbc
aj <134237782+ajtabjs@users.noreply.github.com>
2026-04-09 16:21:05 +0000

    make thing of facts, borrowed from the gays' dot studio page

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const globalposts = "https://bliish.com/api/v1/posts"
 
 const wallposts = `https://bliish.com/api/v1/profiles/${user}/wall/posts`
 
+ const facts = [
+    "test1",
+"test2"
+    ];
+
+    const random = quotes[Math.floor(Math.random() * quotes.length)];
+
 axios.post(globalposts, payload)
 .then(response => {
 console.log('cool status', response.status);
